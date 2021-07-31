460a3566be473ba3daa55523dc2c2e7c38d6e664
Zack Shainsky <zshainsky@gmail.com>
2021-07-30 22:02:52 -0700

    updated iterations

--- iteration/iteration.go
+++ iteration/iteration.go
@@ -1,7 +1,5 @@
 package iteration
 
-const repeatCount = 5
-
 // Repeated function returns a string with 5 iterations of the input character
 func Repeat(character string, numIterations int) string {
 	var repeated string
